9a2b263397efe6c9532349858ed7f36496353c42
Suyalvivek <suyal10vivek@gmail.com>
2025-08-07 13:05:59 +0530

    Fixed some user api

--- front-end/src/modules/user/api/user-api.ts
+++ front-end/src/modules/user/api/user-api.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-const USER_API = import.meta.env.VITE_API_BASE_URL;
-axios.defaults.baseURL = USER_API;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.baseURL = API_BASE_URL;
 
 export const doRegister = (userData: unknown) => {
-  return axios.post('/register', userData);
+  return axios.post('register', userData);
 };
 
 export const doLogin = (userData: unknown) => {
-  return axios.post('/login', userData);
+  return axios.post('login', userData);
 };
+ 
+
